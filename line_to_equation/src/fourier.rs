7652d25b65ad86361a57284f48928d2ac98f630b
Hunter Chen <0x@hunterchen.ca>
2024-01-22 23:43:07 -0500

    it works!
    now to support more images and more than 1 line
    
    have better pathfinding, but I think it works better just to detect more islands (contiguous lines)

--- line_to_equation/src/fourier.rs
+++ line_to_equation/src/fourier.rs
@@ -1,21 +1,41 @@
 use std::f64::consts::PI;
-use num::Complex;
 
-pub fn construct_equation(points: &[Complex<f64>]) -> String {
+pub fn construct_equation(points: &[(i32,i32)]) -> String {
   let mut equation = String::new();
   let mut xt = vec![];
   let mut yt = vec![];
-  for num in points.iter().enumerate() {
-    let (a, b) = (num.1.re, num.1.im);
-    let pin2 = 2.0 * PI * num.0 as f64;
-    xt.push(format!("({} * cos({}t) - {} * sin({}t))", a, pin2, b, pin2));
-    yt.push(format!("({} * sin({}t) + {} * cos({}t))", a, pin2, b, pin2));
+
+  let iterations = 25;
+
+  let mut new_points = points.to_vec();
+
+  for points in points.iter().rev() {
+    new_points.push((points.0, points.1));
+  }
+
+  let len = new_points.len() as f64;
+
+  let (mut cx, mut cy) = (0.0, 0.0);
+  for n in 0..(iterations*2 + 1) {
+    (cx, cy) = (0.0, 0.0);
+    let k = (n - iterations) as f64; // goes from -iterations to iterations
+    let pik2 = 2.0 * PI * k;
+
+    for (i, p) in new_points.iter().enumerate() {
+      let f_num = (p.0 as f64, p.1 as f64);
+      let m = i as f64;
+      cx += (pik2 * m / len).cos() * f_num.0 + (pik2 * m / len).sin() * f_num.1;
+      cy += (pik2 * m / len).cos() * f_num.1 - (pik2 * m / len).sin() * f_num.0;
+    }
+
+    xt.push(format!("{} cos({}t) - {} sin({}t)", cx / len, k * PI, cy / len, k * PI));
+    yt.push(format!("{} sin({}t) + {} cos({}t)", cx / len, k * PI, cy / len, k * PI));
   }
   // println!("xt: {:?}", xt);
   // println!("yt: {:?}", yt);
   equation.push_str("((");
   equation.push_str(&xt.join("+"));
-  equation.push_str("),(");
+  equation.push_str("),-(");
   equation.push_str(&yt.join("+"));
   equation.push_str("))");
   equation
--- line_to_equation/src/main.rs
+++ line_to_equation/src/main.rs
@@ -1,20 +1,24 @@
 mod img_to_line;
 mod fourier;
-use num::complex::Complex;
-use rustfft::FftPlanner;
 
 fn main() {
-  let line = img_to_line::img_to_line("images/checkmark.png");
+  let line = img_to_line::img_to_line("images/eg1.png");
   img_to_line::line_to_img(&line); // to check generated line
-  let mut complex_line = line.iter().map(|&(x,y)| Complex::new(x as f64, y as f64)).collect::<Vec<Complex<f64>>>();
+  // println!("Line: {:?}", line);
+  // println!("cos: {:?}", (10_f32).cos());
+  // let mut complex_line = line.iter().map(|&(x,y)| Complex::new(x as f64, y as f64)).collect::<Vec<Complex<f64>>>();
   // println!("Line: {:?}", line);
   // println!("Complex Line: {:?}", complex_line);
+  
+  let skip = 7;
+  let skipped_line: Vec<(i32,i32)> = line.iter().skip(skip - 1).step_by(skip).copied().collect();
+  // img_to_line::line_to_img(&skipped_line.iter().map(&|c: &Complex<f64>| (c.re as i32, c.im as i32)).collect::<Vec<(i32, i32)>>());
+  // let mut planner: FftPlanner<f64>  = FftPlanner::new();
+  // let fft = planner.plan_fft_forward(skipped_line.len());
 
-  let mut planner: FftPlanner<f64>  = FftPlanner::new();
-  let fft = planner.plan_fft_forward(line.len());
+  // fft.process(&mut skipped_line);
 
-  fft.process(&mut complex_line);
   // println!("FFT: {:?}", complex_line);
-  let equation = fourier::construct_equation(&complex_line);
+  let equation = fourier::construct_equation(&line);
   println!("Equation: {}", equation);
 }
