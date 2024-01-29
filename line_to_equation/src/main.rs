454785a5803949e3108129dd0afb2925bbf2fd71
Hunter Chen <0x@hunterchen.ca>
2024-01-29 01:35:25 -0500

    added threads to allow increased stack size

--- line_to_equation/src/main.rs
+++ line_to_equation/src/main.rs
@@ -3,32 +3,39 @@ mod fourier;
 mod edge_detection;
 use std::{fs::File, io::Write};
 use std::time::Instant;
+use std::thread;
 
 fn main() -> std::io::Result<()> {
-    let img = img_to_line::get_image("images/islands1.png");
+    let builder = thread::Builder::new().stack_size(32 * 1024 * 1024);
 
-    let now = Instant::now();
-    let mut edges = edge_detection::sobel(&img);
-    println!("Sobel: {:?}", now.elapsed());
-    edges.save("images/generated/edges.png").unwrap();
+    let handler = builder.spawn(|| {
+        let img = img_to_line::get_image("images/smile.png");
 
-    let now = Instant::now();
-    let lines = img_to_line::img_to_lines(&mut edges);
-    println!("Img to lines: {:?}", now.elapsed());
-    img_to_line::lines_to_img(&lines);
+        let now = Instant::now();
+        let mut edges = edge_detection::sobel(&img);
+        println!("Sobel: {:?}", now.elapsed());
+        edges.save("images/generated/edges.png").unwrap();
 
-    
-    let mut file = File::create("images/generated/equations.txt").unwrap();
+        let now = Instant::now();
+        let lines = img_to_line::edges_to_lines_b(&mut edges);
+        println!("Img to lines: {:?}", now.elapsed());
+        img_to_line::lines_to_img(&lines);
 
-    let mut equations = Vec::new();
-    for line in lines.iter() {
-        let equation = fourier::construct_equation(line);
-        let _ = file.write(equation.as_bytes());
-        let _ = file.write(b"\n");
-        equations.push(equation);
-    // println!("Equation: {}", equation);
-    }
+        
+        let mut file = File::create("images/generated/equations.txt").unwrap();
 
-    // println!("{:?}",equations);
+        let now = Instant::now();
+        let mut equations = Vec::new();
+        for line in lines.iter() {
+            let equation = fourier::construct_equation(line);
+            let _ = file.write(equation.as_bytes());
+            let _ = file.write(b"\n");
+            equations.push(equation);
+        // println!("Equation: {}", equation);
+        }
+        println!("Construct equations: {:?}", now.elapsed());
+    }).unwrap();
+
+    handler.join().unwrap();
     Ok(())
 }
