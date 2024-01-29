2144e23518b068fa8c677abd98cd0e95d0e201ee
Hunter Chen <0x@hunterchen.ca>
2024-01-29 01:35:11 -0500

    added pathing optimization

--- line_to_equation/src/img_to_line.rs
+++ line_to_equation/src/img_to_line.rs
@@ -1,3 +1,4 @@
+
 use image::{GenericImageView, DynamicImage, GenericImage};
 use rand::Rng;
 
@@ -54,39 +55,79 @@ fn oob(x: i32, y: i32, img: &DynamicImage) -> bool {
   x < 0 || y < 0 || x as u32 >= img.width() || y as u32 >= img.height()
 }
 
-fn dfs(x: i32, y: i32, visited: &mut [bool], img: &DynamicImage, path: &mut Vec<(i32, i32)>) {
-  if visited[(y * img.width() as i32 + x) as usize] {
-    path.push((x, y));
+fn dfs(x: i32, y: i32, visited: &mut [bool], img: &DynamicImage, path: &mut Vec<(i32, i32)>, col: image::Rgba<u8>) {
+  if oob(x, y, img) || img.get_pixel(x as u32, y as u32) != col { 
+    // if out of bounds or already visited or not white
     return;
   }
-  if oob(x, y, img) || img.get_pixel(x as u32, y as u32) != WHITE { 
-    // if out of bounds or already visited or not white
+  if visited[(y * img.width() as i32 + x) as usize] {
+    path.push((x, y));
     return;
   }
   path.push((x, y)); // add to path
   visited[(y * img.width() as i32 + x) as usize] = true; // set visited
   for (i, j) in [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)] { // loop through surrounding 3x3
-    dfs(x + i, y + j, visited, img, path);
+    dfs(x + i, y + j, visited, img, path, col);
   }
 }
 
-pub fn img_to_lines(img: &mut DynamicImage) -> Vec<Vec<(i32, i32)>> {
+fn is_palindrome(s: usize, e: usize, path: &[(i32,i32)]) -> bool {
+  let (mut start, mut end) = (s, e);
+  while start < end {
+    if path[start] != path[end] { return false; }
+    start += 1;
+    end -= 1;
+  }
+  true
+}
+
+fn remove_end_palindrome(path: &mut Vec<(i32, i32)>) {
+  let longest_palindrome = (0..path.len()).rev().find(|&i| is_palindrome(0, i, path));
+  if let Some(i) = longest_palindrome {
+    path.truncate(path.len() - i / 2);
+  }
+}
+
+fn remove_start_palindrome(path: &mut Vec<(i32, i32)>) {
+  let longest_palindrome = (0..path.len()).rev().find(|&i| is_palindrome(0, i, path));
+  if let Some(i) = longest_palindrome {
+    // println!("i: {}", i);
+    path.drain(0..i / 2);
+  }
+}
+
+pub fn edges_to_lines(img: &mut DynamicImage, col: image::Rgba<u8>) -> Vec<Vec<(i32, i32)>> {
   let mut lines = vec![];
   let dims = img.dimensions();
   let mut visited = vec![false; (dims.0 * dims.1) as usize];
 
   for x in 0..dims.0 {
     for y in 0..dims.1 {
-      if !visited[(y * dims.0 + x) as usize] && img.get_pixel(x, y) == WHITE {
+      if !visited[(y * dims.0 + x) as usize] && img.get_pixel(x, y) == col {
         let mut path = vec![];
-        dfs(x as i32, y as i32, &mut visited, img, &mut path);
-        if path.len() > 4 {
+        dfs(x as i32, y as i32, &mut visited, img, &mut path, col);
+        // somehow neither of these seem to do anything but they work in tests
+        // the idea is that if dfs backtracks to a point that is already in the path
+        // from the end of the path, then it can be partially truncated
+        remove_start_palindrome(&mut path);
+        remove_end_palindrome(&mut path);        
+        if path.len() > 32 {
           lines.push(path);
         }
       }
     }
   }
   lines
+}
+
+#[allow(dead_code)]
+pub fn edges_to_lines_w(img: &mut DynamicImage) -> Vec<Vec<(i32, i32)>> {
+  edges_to_lines(img, WHITE)
+}
+
+#[allow(dead_code)]
+pub fn edges_to_lines_b(img: &mut DynamicImage) -> Vec<Vec<(i32, i32)>> {
+  edges_to_lines(img, BLACK)
 }
 
 fn random_col() -> image::Rgba<u8> {
@@ -121,3 +162,50 @@ pub fn lines_to_img(lines: &[Vec<(i32, i32)>]) {
   }
   img.save("images/generated/lines.png").unwrap();
 } // fn lines_to_img()
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_remove_end_palindrome() {
+    let mut path1 = vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6)];
+    remove_end_palindrome(path1.as_mut());
+    assert_eq!(path1, vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6)]);
+  }
+
+  #[test]
+  fn test_remove_end_palindrome2() {
+    let mut path2 = vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(5,5),(4,4),(3,3),(2,2),(1,1),(0,0)];
+    remove_end_palindrome(path2.as_mut());
+    assert_eq!(path2, vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6)]);
+  }
+
+  #[test]
+  fn test_remove_end_palindrome3() {
+    let mut path2 = vec![(0,0),(1,1),(0,1),(2,2),(3,3),(4,4),(5,5),(6,6),(5,5),(4,4),(3,3),(2,2),(0,1),(1,1),(0,0)];
+    remove_end_palindrome(path2.as_mut());
+    assert_eq!(path2, vec![(0,0),(1,1),(0,1),(2,2),(3,3),(4,4),(5,5),(6,6)]);
+  }
+
+  #[test]
+  fn test_remove_start_palindrome() {
+    let mut path1 = vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6)];
+    remove_start_palindrome(path1.as_mut());
+    assert_eq!(path1, vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6)]);
+  }
+
+  #[test]
+  fn test_remove_start_palindrome2() {
+    let mut path2 = vec![(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(5,5),(4,4),(3,3),(2,2),(1,1),(0,0)];
+    remove_start_palindrome(path2.as_mut());
+    assert_eq!(path2, vec![(6,6),(5,5),(4,4),(3,3),(2,2),(1,1),(0,0)]);
+  }
+
+  #[test]
+  fn test_remove_start_palindrome3() {
+    let mut path2 = vec![(0,0),(1,1),(0,0),(2,2),(3,3),(4,4),(5,5),(6,6),(5,5),(4,4),(3,3),(2,2),(1,1),(0,0)];
+    remove_start_palindrome(path2.as_mut());
+    assert_eq!(path2, vec![(1,1),(0,0),(2,2),(3,3),(4,4),(5,5),(6,6),(5,5),(4,4),(3,3),(2,2),(1,1),(0,0)]);
+  }
+}
