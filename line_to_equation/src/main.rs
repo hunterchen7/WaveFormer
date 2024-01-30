7e360276ab5fa0af7d5c086ed019c80a37cfc41b
Hunter Chen <0x@hunterchen.ca>
2024-01-30 03:51:32 -0500

    switch main back to working on single pixel lines to fix DFS issues

--- line_to_equation/src/main.rs
+++ line_to_equation/src/main.rs
@@ -11,13 +11,13 @@ fn main() -> std::io::Result<()> {
     let handler = builder.spawn(|| {
         let img = img_to_line::get_image("images/smile.png");
 
-        let now = Instant::now();
+        /*let now = Instant::now();
         let mut edges = edge_detection::sobel(&img);
         println!("Sobel: {:?}", now.elapsed());
-        edges.save("generated/edges.png").unwrap();
+        edges.save("generated/edges.png").unwrap();*/
 
         let now = Instant::now();
-        let lines = img_to_line::edges_to_lines_w(&mut edges);
+        let lines = img_to_line::edges_to_lines_b(&mut img);
         println!("Img to lines: {:?}", now.elapsed());
         img_to_line::lines_to_img(&lines);
 
@@ -26,7 +26,9 @@ fn main() -> std::io::Result<()> {
         let now = Instant::now();
         let mut equations = Vec::new();
         for line in lines.iter() {
-            let equation = fourier::construct_equation(line);
+            // let equation = fourier::construct_equation(line);
+            // construct equation but it only uses every nth point
+            let equation = fourier::construct_equation(&line.iter().step_by(10).map(|p| *p).collect::<Vec<_>>());
             let _ = file.write(equation.as_bytes());
             let _ = file.write(b"\n");
             equations.push(equation);
