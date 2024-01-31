87a1f681a18671f78a8fb2355c480053a4350e51
Hunter Chen <0x@hunterchen.ca>
2024-01-31 05:41:03 -0500

    added gaussian blur function
    for canny edge detection

--- line_to_equation/src/edge_detection.rs
+++ line_to_equation/src/edge_detection.rs
@@ -23,13 +23,55 @@ pub fn sobel(img: &DynamicImage) -> DynamicImage {
       }
 
       let g = ((gx.pow(2) + gy.pow(2)) as f32).sqrt() as u8;
-      if g > 128 && x > 0 && y > 0 { // threshold for white
+      /*if g > 128 && x > 0 && y > 0 { // threshold for white
         new_img.put_pixel(x, y, image::Rgba([255, 255, 255, 255]));
       } else {
         new_img.put_pixel(x, y, image::Rgba([0, 0, 0, 255]));
-      }
+      }*/
+        new_img.put_pixel(x, y, image::Rgba([g, g, g, 255]));
     }
   }
   new_img
 }
 
+const GAUSSIAN_KERNEL_5X5: [[f32; 5]; 5] = [
+  [1.0, 4.0, 6.0, 4.0, 1.0],
+  [4.0, 16.0, 24.0, 16.0, 4.0],
+  [6.0, 24.0, 36.0, 24.0, 6.0],
+  [4.0, 16.0, 24.0, 16.0, 4.0],
+  [1.0, 4.0, 6.0, 4.0, 1.0]
+];
+pub fn gaussian_blur_5x5(img: &DynamicImage) -> DynamicImage {
+  let mut new_img = img.clone();
+    let kernel = GAUSSIAN_KERNEL_5X5;
+
+  for x in 0..img.width() {
+    for y in 0..img.height() {
+      let mut r = 0.0;
+      let mut g = 0.0;
+      let mut b = 0.0;
+
+      for i in 0..5 {
+        for j in 0..5 {
+          let new_x = ((x as i32 + i - 2).max(0).min(img.width() as i32 - 1)) as u32;
+          let new_y = ((y as i32 + j - 2).max(0).min(img.height() as i32 - 1)) as u32;
+
+          let pixel = img.get_pixel(new_x, new_y);
+          let kernel_val = kernel[i as usize][j as usize];
+
+          r += pixel[0] as f32 * kernel_val;
+          g += pixel[1] as f32 * kernel_val;
+          b += pixel[2] as f32 * kernel_val;
+        }
+      }
+
+      // normalize
+      r /= 255.0;
+      g /= 255.0;
+      b /= 255.0;
+
+      new_img.put_pixel(x, y, image::Rgba([r as u8, g as u8, b as u8, 255]));
+    }
+  }
+  new_img
+}
