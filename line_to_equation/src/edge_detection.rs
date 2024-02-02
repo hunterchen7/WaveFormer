830daa9c91d7be153e3ef83ce280fd87c4f4fd97
Hunter Chen <0x@hunterchen.ca>
2024-02-02 07:57:19 -0500

    gaussian blur micro-optimization
    tried to optimize, didn't really work , but I think the code looks nicer now
    
    Gaussian blur new: 49.3449ms
    Gaussian blur old: 49.1205ms
    
    it's like somewhat consistently ~1% faster on a 4000x6000 image though, no idea why, perhaps a reason to go into the compiled source code and see what's going on.
    
    Gaussian blur new: 2.1261069s
    Gaussian blur old: 2.15414s

--- line_to_equation/src/edge_detection.rs
+++ line_to_equation/src/edge_detection.rs
@@ -48,10 +48,24 @@ pub fn sobel_default(img: &DynamicImage) -> DynamicImage {
   sobel_threshold(img, 128, false)
 }
 
-pub fn gaussian_blur(img: &DynamicImage, kernel: &[f64]) -> DynamicImage {
-  let mut new_img = img.clone();
-  let size = kernel.len().sqrt() as i32; // square matrix
+enum GaussianFilter {
+  K3x3([f64; 9]),
+  K5x5([f64; 25]),
+  K7x7([f64; 49]),
+}
 
+fn gaussian_blur(img: &DynamicImage, kernel: GaussianFilter) -> DynamicImage {
+  let mut new_img = img.clone();
+  match kernel {
+    GaussianFilter::K3x3(k) => apply_kernel(img, &mut new_img, &k),
+    GaussianFilter::K5x5(k) => apply_kernel(img, &mut new_img, &k),
+    GaussianFilter::K7x7(k) => apply_kernel(img, &mut new_img, &k),
+  }
+  new_img
+}
+
+pub fn apply_kernel<const S: usize>(img: &DynamicImage, new_img: &mut DynamicImage, kernel: &[f64; S]) {
+  let size = S.sqrt() as i32;
   for x in 0..img.width() {
     for y in 0..img.height() {
       let mut r = 0.0;
@@ -75,45 +89,88 @@ pub fn gaussian_blur(img: &DynamicImage, kernel: &[f64]) -> DynamicImage {
       new_img.put_pixel(x, y, image::Rgba([r as u8, g as u8, b as u8, 255]));
     }
   }
-  new_img
 }
 
 // Gaussian filtering for FPGA based image processing with High-Level Synthesis tools -
 // Scientific Figure on ResearchGate. Available from:
 // https://www.researchgate.net/figure/Discrete-approximation-of-the-Gaussian-kernels-3x3-5x5-7x7_fig2_325768087
 // [accessed 31 Jan, 2024]
-#[allow(dead_code)]
-pub fn gaussian_blur_5x5(img: &DynamicImage) -> DynamicImage {
-  gaussian_blur(img, &[
-      1.0, 4.0, 7.0, 4.0, 1.0,
-      4.0, 16.0, 26.0, 16.0, 4.0,
-      7.0, 26.0, 41.0, 26.0, 7.0,
-      4.0, 16.0, 26.0, 16.0, 4.0,
-      1.0, 4.0, 7.0, 4.0, 1.0
-    ].iter().map(|x| x / 273.0).collect::<Vec<f64>>()
-  )
-}
+const GAUSSIAN_3X3: [f64; 9] = [
+  1.0 / 16.0, 2.0 / 16.0, 1.0 / 16.0,
+  2.0 / 16.0, 4.0 / 16.0, 2.0 / 16.0,
+  1.0 / 16.0, 2.0 / 16.0, 1.0 / 16.0
+];
+
+const GAUSSIAN_5X5: [f64; 25] = [
+  1.0 / 273.0, 4.0 / 273.0, 7.0 / 273.0, 4.0 / 273.0, 1.0 / 273.0,
+  4.0 / 273.0, 16.0 / 273.0, 26.0 / 273.0, 16.0 / 273.0, 4.0 / 273.0,
+  7.0 / 273.0, 26.0 / 273.0, 41.0 / 273.0, 26.0 / 273.0, 7.0 / 273.0,
+  4.0 / 273.0, 16.0 / 273.0, 26.0 / 273.0, 16.0 / 273.0, 4.0 / 273.0,
+  1.0 / 273.0, 4.0 / 273.0, 7.0 / 273.0, 4.0 / 273.0, 1.0 / 273.0
+];
+
+const GAUSSIAN_7X7: [f64; 49] = [
+  0.0 / 1003.0, 0.0 / 1003.0, 1.0 / 1003.0, 2.0 / 1003.0, 1.0 / 1003.0, 0.0 / 1003.0, 0.0 / 1003.0,
+  0.0 / 1003.0, 3.0 / 1003.0, 13.0 / 1003.0, 22.0 / 1003.0, 13.0 / 1003.0, 3.0 / 1003.0, 0.0 / 1003.0,
+  1.0 / 1003.0, 13.0 / 1003.0, 59.0 / 1003.0, 97.0 / 1003.0, 59.0 / 1003.0, 13.0 / 1003.0, 1.0 / 1003.0,
+  2.0 / 1003.0, 22.0 / 1003.0, 97.0 / 1003.0, 159.0 / 1003.0, 97.0 / 1003.0, 22.0 / 1003.0, 2.0 / 1003.0,
+  1.0 / 1003.0, 13.0 / 1003.0, 59.0 / 1003.0, 97.0 / 1003.0, 59.0 / 1003.0, 13.0 / 1003.0, 1.0 / 1003.0,
+  0.0 / 1003.0, 3.0 / 1003.0, 13.0 / 1003.0, 22.0 / 1003.0, 13.0 / 1003.0, 3.0 / 1003.0, 0.0 / 1003.0,
+  0.0 / 1003.0, 0.0 / 1003.0, 1.0 / 1003.0, 2.0 / 1003.0, 1.0 / 1003.0, 0.0 / 1003.0, 0.0 / 1003.0
+];
 
 #[allow(dead_code)]
 pub fn gaussian_blur_3x3(img: &DynamicImage) -> DynamicImage {
-  gaussian_blur(img, &[
-      1.0, 2.0, 1.0,
-      2.0, 4.0, 2.0,
-      1.0, 2.0, 1.0
-    ].iter().map(|x| x / 16.0).collect::<Vec<f64>>()
-  )
+  gaussian_blur(img, GaussianFilter::K3x3(GAUSSIAN_3X3))
+}
+
+#[allow(dead_code)]
+pub fn gaussian_blur_5x5(img: &DynamicImage) -> DynamicImage {
+  gaussian_blur(img, GaussianFilter::K5x5(GAUSSIAN_5X5))
 }
 
 #[allow(dead_code)]
 pub fn gaussian_blur_7x7(img: &DynamicImage) -> DynamicImage {
-  gaussian_blur(img, &[
-      0.0, 0.0, 1.0, 2.0, 1.0, 0.0, 0.0,
-      0.0, 3.0, 13.0, 22.0, 13.0, 3.0, 0.0,
-      1.0, 13.0, 59.0, 97.0, 59.0, 13.0, 1.0,
-      2.0, 22.0, 97.0, 159.0, 97.0, 22.0, 2.0,
-      1.0, 13.0, 59.0, 97.0, 59.0, 13.0, 1.0,
-      0.0, 3.0, 13.0, 22.0, 13.0, 3.0, 0.0,
-      0.0, 0.0, 1.0, 2.0, 1.0, 0.0, 0.0
-    ].iter().map(|x| x / 1003.0).collect::<Vec<f64>>()
+  gaussian_blur(img, GaussianFilter::K7x7(GAUSSIAN_7X7))
+}
+
+fn gaussian_blur_old(img: &DynamicImage, kernel: &Vec<f64>) -> DynamicImage {
+  let mut new_img = img.clone();
+  let size = kernel.len().sqrt() as i32;
+  for x in 0..img.width() {
+    for y in 0..img.height() {
+      let mut r = 0.0;
+      let mut g = 0.0;
+      let mut b = 0.0;
+
+      for i in 0..size {
+        for j in 0..size {
+          let new_x = ((x as i32 + i - size / 2).max(0).min(img.width() as i32 - 1)) as u32;
+          let new_y = ((y as i32 + j - size / 2).max(0).min(img.height() as i32 - 1)) as u32;
+
+          let pixel = img.get_pixel(new_x, new_y);
+          let kernel_val = kernel[(i * size + j) as usize];
+
+          r += pixel[0] as f64 * kernel_val;
+          g += pixel[1] as f64 * kernel_val;
+          b += pixel[2] as f64 * kernel_val;
+        }
+      }
+
+      new_img.put_pixel(x, y, image::Rgba([r as u8, g as u8, b as u8, 255]));
+    }
+  }
+  new_img
+}
+pub fn gaussian_blur_7x7_old(img: &DynamicImage) -> DynamicImage {
+  gaussian_blur_old(img, &[
+    0.0, 0.0, 1.0, 2.0, 1.0, 0.0, 0.0,
+    0.0, 3.0, 13.0, 22.0, 13.0, 3.0, 0.0,
+    1.0, 13.0, 59.0, 97.0, 59.0, 13.0, 1.0,
+    2.0, 22.0, 97.0, 159.0, 97.0, 22.0, 2.0,
+    1.0, 13.0, 59.0, 97.0, 59.0, 13.0, 1.0,
+    0.0, 3.0, 13.0, 22.0, 13.0, 3.0, 0.0,
+    0.0, 0.0, 1.0, 2.0, 1.0, 0.0, 0.0
+  ].iter().map(|x| x / 1003.0).collect::<Vec<f64>>()
   )
 }
